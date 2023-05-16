47fe3e40e4159e6d4e1f4616717a351c072223b2
Jessica Spivak <jls103193@yahoo.com>
2023-05-16 15:34:58 -0400

    trying to figure out menu button

--- main.js
+++ main.js
@@ -3,8 +3,14 @@
 //     dropdownMenu.classList.toggle('links')
 // })
 
-const mainMenuLink = document.querySelector('#mainMenu');
-const dropdownMenu = document.querySelector('.menu');
-mainMenuLink.addEventListener('click', function(event) {
-  dropdownMenu.classList.toggle('hidden')
+// const mainMenuLink = document.querySelector('#mainMenu');
+// const dropdownMenu = document.querySelector('.menu');
+// mainMenuLink.addEventListener('click', function(event) {
+//   dropdownMenu.classList.toggle('hidden')
+// })
+
+const menuButton = document.querySelector('#menu');
+const menuLinks = document.querySelector('.links');
+menuButton.addEventListener('click', function(event) {
+    menuLinks.classList.toggle('menuHidden')
 })
