3f4b5a7741d37169fc82674788efb3c8c57c3c69
Jessica Spivak <jls103193@yahoo.com>
2023-06-10 09:07:58 -0400

    trying to fix menu button. it either shows with both big and small screen, or doesn't show at all on big or small screen. need to figure this out to get it to work on my other project

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// const navBar = document.querySelector("#top");
-// navBar.addEventListener('click', function(event) {
-//     dropdownMenu.classList.toggle('links')
-// })
-
-// const mainMenuLink = document.querySelector('#mainMenu');
-// const dropdownMenu = document.querySelector('.menu');
-// mainMenuLink.addEventListener('click', function(event) {
-//   dropdownMenu.classList.toggle('hidden')
-// })
-
 const menuButton = document.querySelector('#menu');
 const menuLinks = document.querySelector('.links');
 menuButton.addEventListener('click', function(event) {
